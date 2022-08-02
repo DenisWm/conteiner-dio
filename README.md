8a733f764329158bdc8436cc0897988cb40cfd9e
DenisWm <denismamoni@hotmail.com>
2022-08-02 17:05:22 -0400

    Docker

---
+++ README.md
@@ -0,0 +1,4 @@
+Docker: Utilização prática no cenário de Microsserviços
+Denis Mamoni Estudando
+
+Criando conteineres para curso!!
---
+++ maquinistas.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dados (
+    MaquinistaID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Cidade varchar(150),
+    Estado varchar(50),
+    Status varchar(20),
+    Host varchar(50)
+);
